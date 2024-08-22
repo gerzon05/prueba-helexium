4dd44344a950e3f28d92d6552bc36d32c40188d8
Gerzon05 <gerzonarletrangel@gmail.com>
2024-08-22 10:00:05 -0500

    chore: update SideNav component layout and styling

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -1,25 +1,21 @@
 <script setup lang="ts">
   import NavLinks from './NavLinks.vue';
-import Logout from '../assets/icons/logout.svg';
+  import Logout from '../assets/icons/logout.svg';
 </script>
 
 <template>
-    <div class="flex h-full flex-col px-3 py-4 md:px-2">
-      <Link
-        class="mb-2 flex h-fit items-center rounded-md bg-gray-100 p-4"
-        href="/"
-      >
-        <h1 class='font-bold text-3xl'>Dashboard</h1>
-      </Link>
-      <div class="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
-        <NavLinks />
-        <div class="hidden h-auto w-full grow rounded-md bg-transparent md:block"></div>
-        <form>
-          <button class="flex px-6 h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-100 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-            <img :src="Logout" alt='logo' />
-            <div class="hidden md:block">Sign Out</div>
-          </button>
-        </form>
-      </div>
+  <div class="flex h-full flex-col px-3 py-4 md:px-2">
+    <Link class="mb-2 flex h-fit items-center rounded-md bg-gray-100 p-4" href="/">
+    <h1 class='font-bold text-3xl'>Dashboard</h1>
+    </Link>
+    <div class="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+      <NavLinks />
+      <div class="hidden h-auto w-full grow rounded-md bg-transparent md:block"></div>
+      <button
+        class="flex px-6 h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-100 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+        <img :src="Logout" alt='logo' />
+        <div class="hidden md:block">Sign Out</div>
+      </button>
     </div>
+  </div>
 </template>
