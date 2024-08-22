ab45a09da8d4ec42deb71e2e99fae9bde4df35fa
Gerzon05 <gerzonarletrangel@gmail.com>
2024-08-22 09:27:09 -0500

    feat: pages layout

---
+++ src/layout/LayoutDefault.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import SideNav from '../components/SideNav.vue';
+</script>
+
+<template>
+    <div class="flex h-screen flex-col md:flex-row md:overflow-hidden">
+        <div class="w-full flex-none md:w-64">
+            <SideNav />
+        </div>
+        <div class="flex-grow p-6 md:overflow-y-auto md:p-12">
+            <slot />
+        </div>
+    </div>
+</template>
